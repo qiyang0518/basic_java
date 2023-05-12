88d96249297a5ef433f3f14db5701d7bffc31215
qiyang <qiyang@126.com>
2023-05-12 15:57:08 +0800

    com commit5

---
+++ treeset/TreeSet_.java
@@ -0,0 +1,30 @@
+package com.treeset;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * @author 齐阳
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+public class TreeSet_
+{
+    public static void main(String[] args)
+    {
+        TreeSet treeSet = new TreeSet();
+        treeSet.add("12");
+        treeSet.add("41");
+        treeSet.add("32");
+        treeSet.add("25");
+        System.out.println(treeSet);
+        //自定义比较,传入一个比较器
+        TreeSet treeSet1 = new TreeSet(new Comparator()
+        {
+            @Override
+            public int compare(Object o1,Object o2)
+            {
+                return ((String) o1).compareTo((String) o2);
+            }
+        });
+    }
+}
